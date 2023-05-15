17ab8f6645ac365feb31ea1dbddb4e8f9d05e9dd
Lorenzo_Lazzari <lorenzo.lazzari98@gmail.com>
2023-05-15 13:58:35 +0200

    updated tes code to test both vcr.newdata and vcr.train and also creating fake random D(x,g) matrixes to test if classmap works

--- sample_test_code/test.R
+++ sample_test_code/test.R
@@ -2,23 +2,19 @@
 # install.packages("xgboost")
 # install.packages("caret")
 # install.packages("mlbench")
-
+# install.packages("palmerpenguins")
 
 # Load the required libraries
 library(xgboost)
 library(caret) #for confusion matrix
-library(mlbench) #for digits
-install.packages("palmerpenguins")
-library(palmerpenguins)
-data(penguins)
 library(palmerpenguins)
 library(dplyr)
 library(tidyverse)
 library(xgboost)
-library(caret)
+
 
 # Load the dataset
-data("DNA")
+data(penguins)
 penguins
 
 # Remove missing values
@@ -55,19 +51,19 @@ train_label_num <- as.integer(train_label) - 1 #Xgboost in multiclass wants inte
 test_label_num <- as.integer(test_label) - 1 #Xgboost in multiclass wants integer starting from 0
 xgb_model <- xgboost(data = train_matrix, label = train_label_num, nrounds = 100, objective="multi:softprob",
                      num_class=num_class)
-xgb_model$pred
+
 
 #
 source("feature_code/R/vcr_custom.R")
 library(classmap)
 
 #model evaluation on train
-train_label_pred_num <- predict(xgb_model, train_matrix)
-train_label_pred <- levels(train_label)[train_label_pred_num + 1]
-train_label_pred <- factor(train_label_pred, levels = levels(train_label))
-train_label_pred
-confusionMatrix(train_label_pred, train_label)
-train_probabilities <- predict(xgb_model, train_matrix, outputmargin = FALSE)
+#train_label_pred_num <- predict(xgb_model, train_matrix)
+#train_label_pred <- levels(train_label)[train_label_pred_num + 1]
+#train_label_pred <- factor(train_label_pred, levels = levels(train_label))
+#train_label_pred
+#confusionMatrix(train_label_pred, train_label)
+#train_probabilities <- predict(xgb_model, train_matrix, outputmargin = FALSE)
 
 train_posteriors = predict(xgb_model,train_matrix,reshape=T)
 train_posteriors = as.data.frame(train_posteriors)
@@ -75,16 +71,22 @@ colnames(train_posteriors) = levels(train_label)
 train_label_pred = apply(train_posteriors,1,function(x) colnames(train_posteriors)[which.max(x)])
 confusionMatrix(as.factor(train_label_pred), train_label)
 
-vcr.out=vcr.custom.train(Xnew=train_matrix, ynew=train_label, train_posteriors)
+vcr.out=vcr.custom.train(y=train_label, train_posteriors)
 silplot(vcr.out)
 
+set.seed(123)
+
+# Generate the matrix
+distance_matrix <- matrix(runif(233 * 3), nrow = 233, ncol = 3)
+vcr.out.withdist=vcr.custom.train(y=train_label, train_posteriors, distToClasses = distance_matrix)
+classmap(vcr.out.withdist, whichclass = 3)
 
 #model evaluation on test
-test_label_pred_num <- predict(xgb_model, test_matrix)
-test_label_pred <- levels(test_label)[test_label_pred_num + 1]
-test_label_pred <- factor(test_label_pred, levels = levels(test_label))
-test_label_pred
-confusionMatrix(test_label_pred, test_label)
+#test_label_pred_num <- predict(xgb_model, test_matrix)
+#test_label_pred <- levels(test_label)[test_label_pred_num + 1]
+#test_label_pred <- factor(test_label_pred, levels = levels(test_label))
+#test_label_pred
+#confusionMatrix(test_label_pred, test_label)
 
 test_posteriors = predict(xgb_model,test_matrix,reshape=T)
 test_posteriors = as.data.frame(test_posteriors)
@@ -92,7 +94,11 @@ colnames(test_posteriors) = levels(test_label)
 test_label_pred = apply(test_posteriors,1,function(x) colnames(test_posteriors)[which.max(x)])
 confusionMatrix(as.factor(test_label_pred), test_label)
 
-vcr.out.test=vcr.custom.newdata(Xnew=test_matrix, ynew=test_label, probs=test_posteriors, vcr.custom.train.out = vcr.out)
+vcr.out.test=vcr.custom.newdata(ynew=test_label, probs=test_posteriors, vcr.custom.train.out = vcr.out)
 silplot(vcr.out.test)
+
+distance_matrix_test <- matrix(runif(100 * 3), nrow = 100, ncol = 3)
+vcr.out.withdisttest=vcr.custom.newdata(ynew = test_label, test_posteriors, newDistToClasses = distance_matrix_test, vcr.custom.train.out = vcr.out.withdist)
+classmap(vcr.out.withdisttest, whichclass = 3)
 
 
